212bf8c0ee6094421fa0d9069cdb173ee3228beb
nikosauter <57616042+nikosauter@users.noreply.github.com>
2023-12-20 17:17:39 +0100

    added skaffold framework

--- publisher/src/publisher_programm.py
+++ publisher/src/publisher_programm.py
@@ -1,13 +1,17 @@
 import csv
 import time
 import json
+from confluent_kafka import Producer
+
+#set up producer with batch.size = 1 so that every message will be directly send to kafka
+producer = Producer({'bootstrap.servers': 'kafka:9092', "client.id" :'publisher', "batch.size": 1})
 
 #read csv-file
 data = open('/opt/venv/src/twitter_sample_data_small.csv', 'r')
 data = csv.reader(data)
 
 #create list with keys for json generation
-keys = ["ID", "Timestamp", "Username", "Text"]
+keys = ["id", "timestamp", "username", "text"]
 
 #go through csv-file
 for row in data:
@@ -20,5 +24,8 @@ for row in data:
     #generate json
     json_object = json.dumps(dictionary, indent = 4)
     print(json_object)
+
+    #send json to kafka (no key specified -> use round robin for paritions which is fine as each post can stand for itself and the order is not really relevant)
+    #producer.produce(topic="posts", value=json_object.encode('utf-8'))
+
     time.sleep(.5)
-    break
