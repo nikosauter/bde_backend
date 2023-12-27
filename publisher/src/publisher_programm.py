778df475ba808f0cb2b3f8a2e0acdc29dd977ecb
Cenk Yagkan <51959071+cy006@users.noreply.github.com>
2023-12-27 10:14:39 +0100

    Kafka and .py communication

--- publisher/src/publisher_programm.py
+++ publisher/src/publisher_programm.py
@@ -3,8 +3,19 @@ import time
 import json
 from confluent_kafka import Producer
 
+
+producer_conf = {
+    'bootstrap.servers': 'kafka-cluster:9092',
+    'batch.size': 1
+}
+
+# Create a Kafka producer instance
+producer = Producer(producer_conf)
+
+
+
 #set up producer with batch.size = 1 so that every message will be directly send to kafka
-producer = Producer({'bootstrap.servers': 'kafka:9092', "client.id" :'publisher', "batch.size": 1})
+#producer = Producer({'bootstrap.servers': 'kafka-cluster:9092', "client.id" :'publisher', "batch.size": 1})
 
 #read csv-file
 data = open('/opt/venv/src/twitter_sample_data_small.csv', 'r')
@@ -23,9 +34,12 @@ for row in data:
 
     #generate json
     json_object = json.dumps(dictionary, indent = 4)
-    print(json_object)
+    #print(json_object)
 
     #send json to kafka (no key specified -> use round robin for paritions which is fine as each post can stand for itself and the order is not really relevant)
-    #producer.produce(topic="posts", value=json_object.encode('utf-8'))
+    producer.produce(topic="posts", value=json_object.encode('utf-8'))
+
+    producer.flush()
 
     time.sleep(.5)
+
