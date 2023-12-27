6dbadf5d99e699d8526099b03a42234dcbbe4173
Schneider, Felix - D0290827 <felix.schneider@dm.de>
2023-12-27 21:22:22 +0100

    add spark app deployment

--- spark-app/spark-app.py
+++ spark-app/spark-app.py
@@ -1,49 +1,58 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.types import LongType, StructType, StringType
-from pyspark.sql.functions import expr, from_unixtime, unix_timestamp, from_utc_timestamp, col, regexp_extract_all, split, array_distinct, lit, explode, window, to_timestamp
+from pyspark.sql.types import LongType, StructField, StructType, StringType
+from pyspark.sql.functions import (
+    column, from_json, expr, from_unixtime, unix_timestamp, regexp_extract_all,
+    explode, window, col, lit
+)
 
 spark = SparkSession.builder \
     .appName("Spark Example") \
     .getOrCreate()
 
-spark.sql("set spark.sql.legacy.timeParserPolicy=LEGACY")
+spark.conf.set("spark.sql.legacy.timeParserPolicy", "LEGACY")
 timestamp_format = "EEE MMM dd HH:mm:ss zzz yyyy"
 
-postSchema = StructType() \
-    .add("id", LongType()) \
-    .add("timestamp_str", StringType()) \
-    .add("user", StringType()) \
-    .add("text", StringType())
+postSchema = StructType([
+    StructField("id", LongType()),
+    StructField("timestamp", StringType()),
+    StructField("user", StringType()),
+    StructField("text", StringType())
+])
 
-data = [
-    (1467810672, "Mon Jun 01 12:44:55 PDT 2009", "scotthamilton", "is upset that he can't update his Facebook by texting it... and might cry as a result School today ..."),
-    (1467810917, "Mon Jun 01 12:44:55 PDT 2009", "mattycus", "@Kenichan I dived many times for the #ball. Managed to save 50% The rest go out of bounds"),
-    (1467811184, "Mon Jun 01 12:44:55 PDT 2009", "ElleCTF", "my whole #body feels itchy and like its on #fire"),
-    (1467811184, "Mon Jun 01 12:44:55 PDT 2009", "ElleCTF", "my whole #body feels itchy and like its on #fire"),
-    (1467811184, "Mon Jun 01 12:44:55 PDT 2009", "ElleCTF", "my whole #body feels itchy and like its on #fire"),
-    (1467811184, "Mon Jun 01 12:44:55 PDT 2009", "ElleCTF", "my whole #body feels itchy and like its on #fire"),
-    (1467811190, "Mon Jun 01 12:44:55 PDT 2009", "ElleCTF", "my whole #body feels itchy and like its on #fire"),
-]
-df = spark.createDataFrame(data, schema=postSchema).drop(col("id"), col("user"))
+postStream = spark \
+    .readStream \
+    .format("kafka") \
+    .option("kafka.bootstrap.servers", "kafka-cluster:9092") \
+    .option("subscribe", "posts") \
+    .option("startingOffsets", "earliest") \
+    .load()
 
-df = df \
-    .withColumn("unix_timestamp", unix_timestamp("timestamp_str", timestamp_format)).withColumn("parsed_timestamp", from_unixtime("unix_timestamp")).withColumn("subtracted_timestamp", expr("parsed_timestamp - interval 9 hours")) \
+posts = postStream.select(
+    from_json(
+        column("value").cast("string"),
+        postSchema
+    ).alias("json")
+).select(
+    column("json.*")
+)
+
+df = posts \
+    .withColumn("unix_timestamp", unix_timestamp("timestamp", timestamp_format)) \
+    .withColumn("parsed_timestamp", from_unixtime("unix_timestamp")) \
+    .withColumn("subtracted_timestamp", expr("parsed_timestamp - INTERVAL 9 HOURS")) \
     .withColumn("hashtags", regexp_extract_all('text', lit(r'(#\w+)'))) \
-    .drop(col("text")) \
-    .select(col("parsed_timestamp"), explode(col("hashtags"))) \
-    .withColumnRenamed("col", "hashtag")
-
-df.show(10)
+    .drop("text") \
+    .select(col("parsed_timestamp"), explode(col("hashtags")).alias("hashtag"))
 
 trendingTopics = df.groupBy(
-    window(
-        col("parsed_timestamp"),
-        "1 minute",
-        "1 minute"
-    ),
+    window(col("parsed_timestamp"), "1 minute", "1 minute"),
     col("hashtag")
 ).count()
 
-df.printSchema()
+consoleDump = trendingTopics \
+    .writeStream \
+    .outputMode("update") \
+    .format("console") \
+    .start()
 
-trendingTopics.orderBy(col("window").desc(), col("count").desc()).show()
+spark.streams.awaitAnyTermination()
