03beae95f6fbddc240178e04f1f723e58c468acb
Schneider, Felix - D0290827 <felix.schneider@dm.de>
2023-12-27 19:36:49 +0100

    parse timestamp

--- spark-app/spark-app.py
+++ spark-app/spark-app.py
@@ -1,39 +1,43 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.types import LongType, StructType, StringType
-from pyspark.sql.functions import unix_timestamp, from_utc_timestamp, col, regexp_extract_all, split, array_distinct, lit, explode, window, to_timestamp
+from pyspark.sql.functions import expr, from_unixtime, unix_timestamp, from_utc_timestamp, col, regexp_extract_all, split, array_distinct, lit, explode, window, to_timestamp
 
 spark = SparkSession.builder \
     .appName("Spark Example") \
     .getOrCreate()
 
+spark.sql("set spark.sql.legacy.timeParserPolicy=LEGACY")
+timestamp_format = "EEE MMM dd HH:mm:ss zzz yyyy"
 
 postSchema = StructType() \
     .add("id", LongType()) \
-    .add("timestamp", StringType()) \
+    .add("timestamp_str", StringType()) \
     .add("user", StringType()) \
     .add("text", StringType())
 
 data = [
-    (1467810672, "2023-12-20 14:30:45", "scotthamilton", "is upset that he can't update his Facebook by texting it... and might cry as a result School today ..."),
-    (1467810917, "2023-12-20 15:30:45", "mattycus", "@Kenichan I dived many times for the #ball. Managed to save 50% The rest go out of bounds"),
-    (1467811184, "2023-12-18 16:30:45", "ElleCTF", "my whole #body feels itchy and like its on #fire"),
-    (1467811184, "2023-12-18 16:30:45", "ElleCTF", "my whole #body feels itchy and like its on #fire"),
-    (1467811184, "2023-12-18 16:30:45", "ElleCTF", "my whole #body feels itchy and like its on #fire"),
-    (1467811184, "2023-12-20 16:30:45", "ElleCTF", "my whole #body feels itchy and like its on #fire"),
-    (1467811190, "2023-12-20 16:30:45", "ElleCTF", "my whole #body feels itchy and like its on #fire"),
+    (1467810672, "Mon Jun 01 12:44:55 PDT 2009", "scotthamilton", "is upset that he can't update his Facebook by texting it... and might cry as a result School today ..."),
+    (1467810917, "Mon Jun 01 12:44:55 PDT 2009", "mattycus", "@Kenichan I dived many times for the #ball. Managed to save 50% The rest go out of bounds"),
+    (1467811184, "Mon Jun 01 12:44:55 PDT 2009", "ElleCTF", "my whole #body feels itchy and like its on #fire"),
+    (1467811184, "Mon Jun 01 12:44:55 PDT 2009", "ElleCTF", "my whole #body feels itchy and like its on #fire"),
+    (1467811184, "Mon Jun 01 12:44:55 PDT 2009", "ElleCTF", "my whole #body feels itchy and like its on #fire"),
+    (1467811184, "Mon Jun 01 12:44:55 PDT 2009", "ElleCTF", "my whole #body feels itchy and like its on #fire"),
+    (1467811190, "Mon Jun 01 12:44:55 PDT 2009", "ElleCTF", "my whole #body feels itchy and like its on #fire"),
 ]
 df = spark.createDataFrame(data, schema=postSchema).drop(col("id"), col("user"))
 
 df = df \
-    .withColumn("timestamp", to_timestamp(col("timestamp").cast("timestamp"))) \
+    .withColumn("unix_timestamp", unix_timestamp("timestamp_str", timestamp_format)).withColumn("parsed_timestamp", from_unixtime("unix_timestamp")).withColumn("subtracted_timestamp", expr("parsed_timestamp - interval 9 hours")) \
     .withColumn("hashtags", regexp_extract_all('text', lit(r'(#\w+)'))) \
     .drop(col("text")) \
-    .select(col("timestamp"), explode(col("hashtags"))) \
+    .select(col("parsed_timestamp"), explode(col("hashtags"))) \
     .withColumnRenamed("col", "hashtag")
+
+df.show(10)
 
 trendingTopics = df.groupBy(
     window(
-        col("timestamp"),
+        col("parsed_timestamp"),
         "1 minute",
         "1 minute"
     ),
