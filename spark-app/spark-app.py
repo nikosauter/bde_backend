7380d57ccca5a74a15684d41ca91c653ba445bca
Schneider, Felix - D0290827 <felix.schneider@dm.de>
2023-12-29 21:35:48 +0100

    aggregate hashtags

--- spark-app/spark-app.py
+++ spark-app/spark-app.py
@@ -1,14 +1,16 @@
+import re
+
 from pyspark.sql import SparkSession
-from pyspark.sql.types import LongType, StructField, StructType, StringType
+from pyspark.sql.types import LongType, StructField, StructType, StringType, ArrayType
 from pyspark.sql.functions import (
     column, from_json, expr, from_unixtime, unix_timestamp, regexp_extract_all,
-    explode, window, col, lit, to_timestamp
+    explode, window, col, lit, to_timestamp, udf
 )
 
 def save_to_database(batch_dataframe, batch_id):
     global db_url, db_schema, db_options
     print(f"Writing batchID {batch_id} to database @ {db_url}")
-    batch_dataframe.distinct().write.jdbc(db_url, db_schema, "overwrite", db_options)
+    batch_dataframe.distinct().write.jdbc(db_url, db_schema, "append", db_options)
 
 db_url = 'jdbc:mysql://my-app-mariadb-service:3306/user_posts'
 db_options = {"user": "root", "password": "mysecretpw"}
@@ -20,6 +22,11 @@ spark = SparkSession.builder \
 
 spark.conf.set("spark.sql.legacy.timeParserPolicy", "LEGACY")
 timestamp_format = "EEE MMM dd HH:mm:ss zzz yyyy"
+
+def extract_hashtags(text):
+    return re.findall(r'\#\w+', text)
+
+extract_hashtags_udf = udf(extract_hashtags, ArrayType(StringType()))
 
 post_schema = StructType([
     StructField("id", LongType()),
@@ -36,7 +43,7 @@ post_stream = spark \
     .option("startingOffsets", "earliest") \
     .load()
 
-transformed_posts = post_stream.select(
+stream_data = post_stream.select(
      from_json(
          column("value").cast("string"),
          post_schema
@@ -45,31 +52,39 @@ transformed_posts = post_stream.select(
      column("json.*")
 )
 
-hashtag_occurrences = transformed_posts \
-     .withColumn("unix_timestamp", unix_timestamp("timestamp", timestamp_format)) \
-     .withColumn("parsed_timestamp", to_timestamp("unix_timestamp")) \
-     .withColumn("hashtags", regexp_extract_all('text', lit(r'(#\w+)'))) \
-     .drop("text") \
-     .select(col("parsed_timestamp"), explode(col("hashtags")).alias("hashtag"))
+stream_data = stream_data \
+    .withColumn("timestamp", unix_timestamp("timestamp", timestamp_format)) \
+    .withColumn("timestamp", to_timestamp("timestamp"))
 
-aggregated_posts = hashtag_occurrences.groupBy(
-     window(col("parsed_timestamp"), "1 hour", "10 minutes"),
-     col("hashtag")
-).count() \
-    .withColumn("post_window_start", col("window.start")) \
-    .withColumn("post_window_end", col("window.end")) \
-    .drop("window", "post_window_start")
+stream_data = stream_data.withColumn("hashtags", extract_hashtags_udf(col("text")))
 
-console_dump = aggregated_posts \
+stream_data = stream_data.select(col("id"), col("timestamp"), col("username"), explode(col("hashtags")).alias("hashtag"))
+
+stream_data = stream_data.withWatermark("timestamp", "1 hour") \
+    .groupBy(
+        window(col("timestamp"), "3 hours", "3 hours"),
+        col("hashtag")
+    ) \
+    .count()
+
+stream_data = stream_data \
+    .withColumnRenamed("count", "counter") \
+    .withColumn("window_start", col("window.start")) \
+    .withColumn("window_end", col("window.end")) \
+    .drop("window")
+
+query = stream_data \
     .writeStream \
-    .outputMode("update") \
+    .outputMode("complete") \
+    .option("numRows", 40) \
+    .option("truncate", False) \
     .format("console") \
     .start()
 
-db_insert_stream = aggregated_posts \
-    .select(column("post_window_end"), column("hashtag"), column('count').alias("counter")) \
+db_insert_stream = stream_data \
+    .select(column("window_start"), column("window_end"), column("hashtag"), column("counter")) \
     .writeStream \
-    .outputMode("update") \
+    .outputMode("complete") \
     .foreachBatch(save_to_database) \
     .start()
 
