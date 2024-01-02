0be653c171d931d38a542116f0755327a326d177
Schneider, Felix - D0290827 <felix.schneider@dm.de>
2024-01-02 22:28:14 +0100

    Add post endpoint

---
+++ backend/README.md
@@ -0,0 +1,5 @@
+Run with minikube:
+1. `minikube addons enable ingress`
+2. `skaffold dev`
+2. `minikube tunnel`
+3. Visit http://localhost/trending in browser
--- backend/src/main.py
+++ backend/src/main.py
@@ -3,8 +3,12 @@ import time
 import json
 import mariadb
 import sys
+import uuid
+import time
+import pytz
+from datetime import datetime
 from confluent_kafka import Producer
-from fastapi import Depends, FastAPI, Header, Request
+from fastapi import Depends, FastAPI, Header, Request, Body
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 from fastapi.templating import Jinja2Templates
@@ -27,6 +31,19 @@ async def get_trending_topics(request: Request):
     trending_hashtags = fetch_trending_hashtags()
     return templates.TemplateResponse("trending.html", {"request": request, "results": trending_hashtags})
 
+@app.post("/post")
+def send_post(post_content: str = Body(..., embed=True)):
+    json_object = {
+        "id": str(uuid.uuid4()),
+        "timestamp": current_datetime(),
+        "username": "dummy_username",
+        "text": post_content
+    }
+    json_object = json.dumps(json_object, indent = 4)
+    producer.produce(topic="posts", value=json_object.encode('utf-8'))
+    producer.flush()
+    return {"message": "Received string: " + post_content}
+
 class CustomJSONEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, datetime):
@@ -44,7 +61,7 @@ def fetch_trending_hashtags():
         )
 
         cursor = conn.cursor()
-        cursor.execute("SELECT * FROM trending_hashtags ORDER BY window_end, counter DESC LIMIT 5")
+        cursor.execute("SELECT * FROM trending_hashtags ORDER BY window_end DESC, counter DESC LIMIT 5")
         rows = cursor.fetchall()
         columns = [column[0] for column in cursor.description]
 
@@ -61,3 +78,11 @@ def fetch_trending_hashtags():
     except mariadb.Error as e:
         print(f"Error connecting to MariaDB: {e}")
         sys.exit(1)
+
+def current_datetime() -> str:
+    utc_datetime = datetime.now(pytz.utc)
+    cet_timezone = pytz.timezone('CET')
+    cet_datetime = utc_datetime.astimezone(cet_timezone)
+    formatted_datetime = cet_datetime.strftime("%a %b %d %H:%M:%S")
+    current_year = datetime.now().year
+    return f"{formatted_datetime} CET {current_year}"
